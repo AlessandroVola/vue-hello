4784801a4cc49cc889214d8a4bc81a9986e31234
AlessandroVola <volaalessandro@gmail.com>
2022-09-12 21:21:36 +0200

    vue added

---
+++ js/script.js
@@ -0,0 +1,14 @@
+console.log('JS-OK')
+
+// Descrizione:
+// Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+
+
+const app = new Vue(
+    {
+        el: '#root',
+        data: {
+
+        }
+    }
+);
